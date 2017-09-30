25c5e429fb902fdbfc7c5db804cab6cfbdc5a132
Justin Trantham <justin.trantham@netgainhosting.com>
2017-09-30 03:34:30 -0500

    First commit

---
+++ LinearCompair/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LinearCompair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.KBColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InstalledColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(673, 102);
+            this.panel1.TabIndex = 0;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkButton.Location = new System.Drawing.Point(0, 0);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(673, 102);
+            this.checkButton.TabIndex = 2;
+            this.checkButton.Text = "Download And Install";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_ClickAsync);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.KBColumn,
+            this.InstalledColumn,
+            this.statusColumn});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(673, 367);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // KBColumn
+            // 
+            this.KBColumn.HeaderText = "KB";
+            this.KBColumn.Name = "KBColumn";
+            this.KBColumn.ReadOnly = true;
+            // 
+            // InstalledColumn
+            // 
+            this.InstalledColumn.HeaderText = "Installed";
+            this.InstalledColumn.Name = "InstalledColumn";
+            this.InstalledColumn.ReadOnly = true;
+            // 
+            // statusColumn
+            // 
+            this.statusColumn.HeaderText = "Status";
+            this.statusColumn.Name = "statusColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 469);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn KBColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InstalledColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn statusColumn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ LinearCompair/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WUApiLib;
+
+namespace LinearCompair
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            initsupportedKB();
+            initLB();
+        }
+
+        private void initsupportedKB()
+        {
+            _supportedKB = new List<string>();
+            
+        }
+
+        private List<string> _supportedKB;
+        private void initLB()
+        {
+            string line;
+
+            // Read the file and display it line by line.  
+            StreamReader file = new StreamReader("MasterRecord.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                dataGridView1.Rows.Add(line);
+                _supportedKB.Add(line);
+            }
+
+            file.Close();
+        }
+
+
+        private async void checkButton_ClickAsync(object sender, EventArgs e)
+        {
+            
+            await Task.Factory.StartNew(() => searcherAsync());
+            
+        }
+
+        
+        private async Task searcherAsync()
+        {
+            UpdateSession uSession = new UpdateSession();
+            IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+            ISearchResult uResult = uSearcher.Search("IsInstalled=0 and Type=Security");
+
+            UpdateCollection updateCollection = new UpdateCollection();
+            string _subString;
+            foreach(IUpdate update in uResult.Updates)
+            {
+                _subString = ExtractString(update.Title);
+                foreach (string kb in _supportedKB)
+                {
+                    if (kb == _subString)
+                    {
+                        gridUpdate(kb, 2, "Found");
+                    }
+                }
+            }
+            foreach (IUpdate update in uResult.Updates)
+            {
+
+                _subString = ExtractString(update.Title);
+                
+                foreach(string kb in _supportedKB)
+                {
+                    if (kb == _subString)
+                    {
+                        updateCollection.Add(update);
+                        gridUpdate(kb, 2, "Downloaded");
+                        await Task.Run(() => download(uSession, updateCollection));
+
+                        gridUpdate(kb, 2, "Downloaded");
+
+                        gridUpdate(kb, 2, "Installing");
+                        install(uSession, updateCollection);
+                        updateCollection.RemoveAt(0);
+                    }
+                }
+            }
+        }
+        private void install(UpdateSession uSession,UpdateCollection updateCollection)
+        {
+            IUpdateInstaller installer = uSession.CreateUpdateInstaller();
+            installer.Updates = updateCollection;
+
+            IInstallationResult installationRes = installer.Install();
+            string _subString;
+            
+            for (int i = 0; i < updateCollection.Count; i++)
+            {
+                
+                _subString = ExtractString(updateCollection[i].Title);
+                if (installationRes.GetUpdateResult(i).HResult == 0)
+                {
+                    gridUpdate(_subString, 2, "Installed");
+                }
+                else
+                {
+                    gridUpdate(_subString, 2, "Failed");
+                }
+            }
+        }
+        private void gridUpdate(string kb,int cell, string text)
+        {
+            dataGridView1.Invoke((MethodInvoker)(() => dataGridView1.Enabled = false));
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                if (kb == dataGridView1.Rows[i].Cells[0].Value.ToString())
+                {
+                    dataGridView1.Invoke((MethodInvoker)(() => dataGridView1.Rows[i].Cells[cell].Value = text));
+                }
+
+            }
+            dataGridView1.Invoke((MethodInvoker)(() => dataGridView1.Enabled = true));
+        }
+        private async Task previousInstallCheck()
+        {
+            var updateSession = new UpdateSession();
+            var updateSearcher = updateSession.CreateUpdateSearcher();
+            var count = updateSearcher.GetTotalHistoryCount();
+            var history = updateSearcher.QueryHistory(0, count);
+
+            dataGridView1.Invoke((MethodInvoker)(() => dataGridView1.Visible = false));
+
+            foreach (string s in _supportedKB)
+            {
+                gridUpdate(s, 1, "Missing");
+                string splitstring;
+                for (int i = 0; i < count; ++i)
+                {
+                    splitstring = ExtractString(history[i].Title);
+                    if(splitstring == s)
+                    {
+                        gridUpdate(s, 1, "Installed");
+                    }
+                }
+                
+            }
+
+            dataGridView1.Invoke((MethodInvoker)(() => dataGridView1.Visible = true));
+        }
+        private void download(UpdateSession uSession,UpdateCollection uCollection)
+        {
+            UpdateDownloader downloader = uSession.CreateUpdateDownloader();
+            downloader.Updates = uCollection;
+            downloader.Download();
+        }
+        string ExtractString(string s)
+        {
+            // You should check for errors in real-world code, omitted for brevity
+            try
+            {
+                var startTag = "(";
+                int startIndex = s.IndexOf(startTag) + startTag.Length;
+                int endIndex = s.IndexOf(")", startIndex);
+                return s.Substring(startIndex, endIndex - startIndex);
+            }
+            catch
+            {
+                return ("CNVFL");
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() => previousInstallCheck());
+        }
+    }
+}
