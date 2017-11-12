57315b0e5fbd45351cea55fd907a9bafeb554a4b
Justin Trantham <justin.trantham@netgainhosting.com>
2017-11-12 13:15:20 -0600

    Changed to folderWatch service, working multi threaded file checker. Running Islock not being used currently

---
+++ Lock.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FolderService
+{
+    class LockandTransfer
+    {
+        
+        public void work(FileInfo fileInfo, string fullPath, string name)
+        {
+            string sub = fullPath.Substring(ConfigurationManager.AppSettings["CopyFromDir"].Length);
+            string[] subFolders = findSubFolders(sub);
+            Service1 s = new Service1();
+            
+            bool isDir = dirCheck(subFolders[subFolders.Length - 1]);
+            
+            try
+            {
+                if (isDir == false)
+                {
+                    bool b = CheckLock(fileInfo);
+                    File.Copy(fullPath, ConfigurationManager.AppSettings["CopyToDir"] + "\\" + name);
+                    s.EventLog.WriteEntry(fullPath + " copied to " + ConfigurationManager.AppSettings["CopyToDir"] + "\\" + name);
+                }
+                else
+                {
+                    Directory.CreateDirectory(ConfigurationManager.AppSettings["CopyToDir"] + sub);
+                    s.EventLog.WriteEntry(fullPath + " Directory created at " + ConfigurationManager.AppSettings["CopyToDir"] + "\\" + name);
+                }
+
+            }
+            catch
+            {
+                s.EventLog.WriteEntry("error copying files");
+            }
+        }
+        
+        bool CheckLock(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            
+
+            return false;
+        }
+        string[] findSubFolders(string sub)
+        {
+            char delimiter = '\\';
+            string[] substrings = sub.Split(delimiter);
+            return substrings;
+        }
+        bool dirCheck(string tester)
+        {
+            bool res = false;
+
+            if (Path.GetExtension(tester) == "")
+            {
+                //checks if file or DIR
+                res = true;
+            }
+            return res;
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        
+        protected override void OnStart(string[] args)
+        {
+            EventLog.WriteEntry("Folder Watch Has started.");
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry("Folder Watch Has Stopped.");
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            //Not Handled
+        }
+        
+
+        private void fileSystemWatcher1_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            
+            LockandTransfer LAT = new LockandTransfer();
+            FileInfo fileInfo = new FileInfo(e.FullPath);
+            BackgroundQueue bgQueue = new BackgroundQueue();
+
+            bgQueue.QueueTask(() => LAT.work(fileInfo,e.FullPath,e.Name));
+            
+            
+        }
+
+        private void fileSystemWatcher1_Deleted(object sender, System.IO.FileSystemEventArgs e)
+        {
+            //not handled
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, System.IO.RenamedEventArgs e)
+        {
+            //not handled
+        }
+    }
+}
